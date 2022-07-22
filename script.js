a6544b2fdc8dc52fd0f6e13e51ee95b04d0d7240
fairusinampratama <fairusinam08@gmail.com>
2022-07-22 17:02:21 +0700

    *

--- script.js
+++ script.js
@@ -1,76 +1,112 @@
-function computerPlay () {
-    const computer = ["ROCK", "PAPER", "SCISSORS"];
-    const random = Math.floor(Math.random() * computer.length);
-    return computer[random];
-}
+const body = () => {
+    let playerScore = 0;
+    let computerScore = 0;
 
-function playRound (playerSelection, computerSelection) {
-    const template = playerSelection.charAt(0).toUpperCase() + playerSelection.toLowerCase().slice(1) + " vs " + computerSelection.charAt(0).toUpperCase() + computerSelection.toLowerCase().slice(1);
-    const win = "Win! " + template;
-    const draw = "Draw! " + template;
-    const lose = "Lose! " + template;
-
-    let Rock = "Rock";
-    let Paper = "Paper";
-    let Scissors = "Scissors";
-
-    let Rocklow = Rock.toLowerCase();
-    let Paperlow = Paper.toLowerCase();
-    let Scissorslow = Scissors.toLowerCase();
-
-    let playerSelectionlow = playerSelection.toLowerCase();
-    let computerSelectionlow = computerSelection.toLowerCase();
-
-    switch(playerSelectionlow) {
-        case Rocklow:
-            switch (computerSelectionlow){
-                case Rocklow:
-                    return draw;
-                    break;
-                case Paperlow:
-                    return lose;
-                    break;
-                case Scissorslow:
-                    return win;
-                    break;
-            }
-            break;
-        case Paperlow:
-            switch (computerSelectionlow){
-                case Rocklow:
-                    return win;
-                    break;
-                case Paperlow:
-                    return draw;
-                    break;
-                case Scissorslow:
-                    return lose;
-                    break;
-            }
-            break;
-        case Scissorslow:
-            switch (computerSelectionlow){
-                case Rocklow:
-                    return lose;
-                    break;
-                case Paperlow:
-                    return win;
-                    break;
-                case Scissorslow:
-                    return draw;
-                    break;
-            }
-            break;
+    function computerPlay () {
+        const computer = ["ROCK", "PAPER", "SCISSORS"];
+        const random = Math.floor(Math.random() * computer.length);
+        return computer[random];
     }
-} 
 
-function game() {
-    for (let i = 0; i < 5; i++) {
-        const playerSelection = prompt("What is your choice?");
+    function playRound (playerSelection, computerSelection) {
+        const gameOver = () => {
+            if(computerScore == 5 || playerScore == 5) {
+                Finish();
+                computerScore = 0;
+                playerScore = 0;
+                playerScoreBoard.innerHTML = playerScore;
+                computerScoreBoard.innerHTML = computerScore;
+            }
+        }
+        
+        let Rock = "Rock";
+        let Paper = "Paper";
+        let Scissors = "Scissors";
+
+        let Rocklow = Rock.toLowerCase();
+        let Paperlow = Paper.toLowerCase();
+        let Scissorslow = Scissors.toLowerCase();
+
+        let playerSelectionlow = playerSelection.toLowerCase();
+        let computerSelectionlow = computerSelection.toLowerCase();
+
+        const computerScoreBoard = document.querySelector('.computerCount');
+        const playerScoreBoard = document.querySelector('.playerCount');
+
+        selection = document.querySelector('.selection');
+        selection.innerHTML = playerSelection.charAt(0).toUpperCase() + playerSelection.toLowerCase().slice(1) + " vs " + computerSelection.charAt(0).toUpperCase() + computerSelection.toLowerCase().slice(1);
+        gameOver();
+
+        switch(playerSelectionlow) {
+            case Rocklow:
+                switch (computerSelectionlow){
+                    case Paperlow:
+                        computerScore++;
+                        computerScoreBoard.innerHTML = computerScore;
+                        break;
+                    case Scissorslow:
+                        playerScore++;
+                        playerScoreBoard.innerHTML = playerScore;
+                        break;
+                }
+                break;
+            case Paperlow:
+                switch (computerSelectionlow){
+                    case Rocklow:
+                        playerScore++;
+                        playerScoreBoard.innerHTML = playerScore;
+                        break;
+                    case Scissorslow:
+                        computerScore++;
+                        computerScoreBoard.innerHTML = computerScore;
+                        break;
+                }
+                break;
+            case Scissorslow:
+                switch (computerSelectionlow){
+                    case Rocklow:
+                        computerScore++;
+                        computerScoreBoard.innerHTML = computerScore;
+                        break;
+                    case Paperlow:
+                        playerScore++;
+                        playerScoreBoard.innerHTML = playerScore;
+                        break;
+                }
+                break;
+        }
+    } 
+
+    function game() {
+        document.getElementById("rock").addEventListener("click", function() {
+            playerSelection = "Rock";
+            content();
+        });
+        document.getElementById("paper").addEventListener("click", function() {
+            playerSelection = "Paper";
+            content();
+        });
+        document.getElementById("scissors").addEventListener("click", function() {
+            playerSelection = "Scissors";
+            content();
+        });
+    }
+
+    function content(){
         const computerSelection = computerPlay();
-        console.log("Round " + (i + 1));
-        console.log(playRound(playerSelection, computerSelection));
+        playRound(playerSelection, computerSelection);
     }
+
+    const Finish = (playerSelection) => {
+        const result = document.querySelector('.result');
+        if(playerScore > computerScore){
+            result.innerText = 'You Won The Game'
+        }
+        else {
+            result.innerText = 'You Lost The Game';
+        }
+    }
+    game();
 }
 
-game();
+body();
